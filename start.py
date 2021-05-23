cc8df3b42e5073d07c587a4c9d7b8caf3026e4d6
Tatyana Egorenkova <tatyana.egorenkova.19@gmail.com>
2021-05-23 21:54:43 +0700

    add constants

--- start.py
+++ start.py
@@ -2,8 +2,13 @@ import pygame
 
 pygame.init()
 
-pygame.display.set_mode((600, 400))
+pygame.display.set_mode((600, 400), pygame.RESIZABLE) # вторая константа позволяет менять параметры окна
+pygame.display.set_caption('The Cool Game')  # меняю заголовок у игрового окна
+pygame.display.set_icon(pygame.image.load('image/app.bmp'))  # меняю иконку у игрового окна
 
+clock = pygame.time.Clock()
+# FPS - Frames per second
+FPS = 60
 flag_running = True
 
 while flag_running:
@@ -12,5 +17,6 @@ while flag_running:
         if event.type == pygame.QUIT:
             pygame.quit()
             flag_running = False
+    clock.tick(FPS)  # количество итераций в секунду (учитывает срок выполнения цикла)
 
 print('Game over')
